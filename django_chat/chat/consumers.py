23b0edb8d48f2aee3c538a4f03600837ac4494cb
Michael Florya <mih2303@mail.ru>
2024-04-13 23:05:09 +0300

    create client api websocket

--- django_chat/chat/consumers.py
+++ django_chat/chat/consumers.py
@@ -4,6 +4,7 @@ from channels.generic.websocket import WebsocketConsumer
 class JoinAndLeave(WebsocketConsumer):
     def connect(self):
         print("server says connected")
+        self.accept()
 
     def receive(self, text_data=None, bytes_data=None):
         print("server says client message received: ", text_data)
