7689be4d445fa8fc44107b36faf611b90f7906a7
Michael Florya <mih2303@mail.ru>
2024-04-13 23:11:31 +0300

    develope button homepage

--- django_chat/chat/consumers.py
+++ django_chat/chat/consumers.py
@@ -1,14 +1,40 @@
+import json
 from channels.generic.websocket import WebsocketConsumer
+from accounts.models import User
+from .models import Event, Message, Group
 
 
 class JoinAndLeave(WebsocketConsumer):
+
     def connect(self):
-        print("server says connected")
+        self.user = self.scope["user"]
         self.accept()
 
     def receive(self, text_data=None, bytes_data=None):
-        print("server says client message received: ", text_data)
-        self.send("Server sends Welcome")
+        text_data = json.loads(text_data)
+        type = text_data.get("type", None)
+        if type:
+            data = text_data.get("data", None)
 
-    def disconnect(self, code):
-        print("server says disconnected")
+        if type == "leave_group":
+            self.leave_group(data)
+        elif type == "join_group":
+            self.join_group(data)
+
+    def leave_group(self, group_uuid):
+        group = Group.objects.get(uuid=group_uuid)
+        group.remove_user_from_group(self.user)
+        data = {
+            "type": "leave_group",
+            "data": group_uuid
+        }
+        self.send(json.dumps(data))
+
+    def join_group(self, group_uuid):
+        group = Group.objects.get(uuid=group_uuid)
+        group.add_user_to_group(self.user)
+        data = {
+            "type": "join_group",
+            "data": group_uuid
+        }
+        self.send(json.dumps(data))
--- django_chat/django_chat/asgi.py
+++ django_chat/django_chat/asgi.py
@@ -9,15 +9,22 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import chat.routing
+from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_channels_chat.settings')
 
 asgi_application = get_asgi_application()
 
 application = ProtocolTypeRouter({
-            "http": asgi_application,
-            "websocket": URLRouter(chat.routing.websocket_urlpatterns)
-                       })
+    "http": asgi_application,
+    "websocket":
+        AllowedHostsOriginValidator(
+            AuthMiddlewareStack(
+                URLRouter(chat.routing.websocket_urlpatterns)
+            ),
+        )
+})
